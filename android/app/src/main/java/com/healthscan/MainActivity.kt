cdbf7a458fab2ee36d0400d34dd83f5ec1c028a4
mam452323574 <malo.houdayer@gmail.com>
2025-11-07 20:24:28 +0100

    Updated package.json

--- android/app/src/main/java/com/healthscan/MainActivity.kt
+++ android/app/src/main/java/com/healthscan/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.helloworld
+package com.healthscan
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/healthscan/MainApplication.kt
+++ android/app/src/main/java/com/healthscan/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.helloworld
+package com.healthscan
 
 import android.app.Application
 import android.content.res.Configuration
